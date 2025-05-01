46cfdee830daa955d945f5db0b2eb62d7d76773f
Ryan Malloy <ryan@supported.systems>
2025-05-01 14:59:31 -0600

    allow passing path to page_view_count

--- tracking/templatetags/tracking.py
+++ tracking/templatetags/tracking.py
@@ -6,9 +6,13 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def page_view_count(context):
-    if request := context.get("request"):
+def page_view_count(context, path=None):
+    if not path:
+        if request := context.get("request"):
+            path = request.path
+    if path:
         return Pageview.objects.filter(
-            url__contains=request.path,
+            url__contains=path,
         ).count()
+
     return None
